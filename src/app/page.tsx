643ec920f13476294f4061feb1531d5d50a291d1
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-08-19 13:08:50 +0600

    search functionality done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 export default function Home() {
+  const [searchText,setSearchText] = useState<string>("")
+  const searchHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event?.target?.value)
+  };
   return (
-    <div>
-      <p className="text-gray-200">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error animi
-        tenetur quod maiores possimus cumque sed itaque atque. Minima ipsam et
-        maxime, harum omnis accusamus aliquid pariatur! Nostrum, perspiciatis
-        id?
-      </p>
+    <div className="w-full">
+      <div className="flex justify-center items-center gap-3 w-full">
+        <input
+          onChange={searchHandle}
+          type="text"
+          name="search"
+          id="search"
+          className="px-3 py-1 rounded"
+          value={searchText}
+        />
+        <button className="flex justify-center items-center shadow-md shadow-sky-500">
+          <AiOutlineSearch className="text-sky-600 w-7 h-7 bg-gray-200 rounded " />
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,12 +1,23 @@
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { FaDev } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="flex justify-center items-center flex-col w-full py-4 font-mono">
       <p className="text-sm text-gray-200">
         Design and developed by -{" "}
-        <a href="https://mahfuzur01.netlify.app/" className="text-sky-700 cursor-pointer">
+        <a
+          href="https://mahfuzur01.netlify.app/"
+          className="text-sky-700 cursor-pointer"
+        >
           Mafuzur Rahman
         </a>
       </p>
+      {/* // ============== icons ========== //  */}
+      <div className="flex justify-center items-center gap-2 mt-4 mb-5">
+        <AiFillLinkedin className=" text-sky-600 w-6 h-6" />
+        <AiFillGithub className=" text-sky-600 w-6 h-6"/>
+        <FaDev className=" text-sky-600 w-6 h-6"/>
+      </div>
     </div>
   );
 };
