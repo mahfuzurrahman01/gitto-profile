9a93d508c829948a981e42e707509713d8dd3b16
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-08-19 12:32:46 +0600

    setup done

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center flex-col w-full py-4 font-mono">
+      <p className="text-sm text-gray-200">
+        Design and developed by -{" "}
+        <a href="https://mahfuzur01.netlify.app/" className="text-sky-700 cursor-pointer">
+          Mafuzur Rahman
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
